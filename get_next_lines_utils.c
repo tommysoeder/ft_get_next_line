225ef93524bfbda81a472b581c67f82d7fcb3e3c
Tomas Martin Soeder <tomamart@c2r7s5.42madrid.com>
2025-05-26 20:00:07 +0200

    prueba malloc strndup

---
+++ get_next_lines_utils.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == ((char)c))
+			return ((char *) s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		str[j++] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[j++] = s2[i];
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
--- pruebas.c
+++ pruebas.c
@@ -1,29 +1,135 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 //#define BUFFER_SIZE 100
 
-int obtain_fd(char *file)
-{
-    int fd;
+// int obtain_fd(char *file)
+// {
+//     int fd;
 
-    fd = open(file, O_RDONLY);
-    return(fd);
+//     fd = open(file, O_RDONLY);
+//     return(fd);
+// }
+// int pruebas_read(int fd, char  *buffer)
+// {
+//     int bytes_read = read(fd, buffer, BUFFER_SIZE);
+//     buffer[bytes_read] = '\0';
+//     return(bytes_read);
+// }
+
+// int main(void)
+// {
+//     char    buffer[BUFFER_SIZE + 1];
+//     int fd;
+
+//     fd = obtain_fd("lorem.txt");
+//     int bytes = pruebas_read(fd, buffer);
+//     printf("Ha leido %d bytes\n", bytes);
+//     printf("En el buffer pone: %s\n", buffer);
+// }
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
-int pruebas_read(int fd, char  *buffer)
+
+// int	main(void)
+// {
+// 	char buffer[100];
+// 	char	*new_buffer;
+// 	int	how_many_bytes;
+// 	int	i;
+
+// 	i = 0;
+// 	how_many_bytes = read(0, buffer, 100);
+// 	new_buffer = malloc(sizeof(char) * (how_many_bytes + 1));
+
+// 	while (i < how_many_bytes)
+// 		{
+// 			new_buffer[i] = buffer[i];
+// 			i++;
+// 		}
+// 	new_buffer[i] = '\0';
+// 	printf("%s\n", new_buffer);
+// 	free(new_buffer);
+// }
+// int	main(void)
+// {
+// 	char	*imprimo;
+// 	imprimo = malloc(sizeof(char) * 5);
+
+// 	imprimo[0] = 'h';
+// 	imprimo[1] = 'o';
+// 	imprimo[2] = 'l';
+// 	imprimo[3] = 'a';
+// 	imprimo[4] = '\0';
+	
+// 	printf("%c", imprimo[3]);
+// 	free(imprimo);
+// }
+
+// char	*ft_strdup(char *src)
+// {
+// 	char	*dst;
+// 	int	i;
+// 	int	len;
+	
+// 	len = ft_strlen(src);
+// 	i = 0;
+// 	dst = malloc(sizeof(char) * len + 1);
+// 	if (!dst)
+// 		return (NULL);
+// 	while (i < len)
+// 		{
+// 			dst[i] = src[i];
+// 			i++;
+// 		}
+// 	dst[i] = '\0';
+// 	return (dst);
+// }
+
+// int	main(void)
+// {
+// 	char *string;
+// 	char *dup;
+
+// 	string = "Hola";
+// 	dup = ft_strdup(string);
+// 	printf("%s\n", dup);
+// 	free(dup);
+// }
+char	*ft_strndup(char *src, size_t n)
 {
-    int bytes_read = read(fd, buffer, BUFFER_SIZE);
-    buffer[bytes_read] = '\0';
-    return(bytes_read);
+	char	*dst;
+	size_t	i;
+	int	len;
+	
+	len = ft_strlen(src);
+	i = 0;
+	dst = malloc(sizeof(char) * n + 1);
+	if (!dst)
+		return (NULL);
+	while (i < n && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+	dst[i] = '\0';
+	return (dst);
 }
 
-int main(void)
+int	main(void)
 {
-    char    buffer[BUFFER_SIZE + 1];
-    int fd;
+	char *string;
+	char *dup;
 
-    fd = obtain_fd("lorem.txt");
-    int bytes = pruebas_read(fd, buffer);
-    printf("Ha leido %d bytes\n", bytes);
-    printf("En el buffer pone: %s\n", buffer);
+	string = "Hola, qué tal";
+	dup = ft_strndup(string, 5);
+	printf("%s\n", dup);
+	free(dup);
 }
